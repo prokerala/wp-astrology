4ae71b1b3da6dcdcd5737e8463d3e1abb2a241b0
Joyce Babu <joyce@ennexa.com>
2021-10-11 18:26:10 +0530

    fix: location parameters not passed correctly

--- assets/src/js/front/main/main.js
+++ assets/src/js/front/main/main.js
@@ -17,20 +17,27 @@ export class Main {
 
 	register( inputs ) {
 		[ ...inputs ].map( function( input ) {
-			const inputPrefix = input.dataset.location_input_prefix ? input.dataset.location_input_prefix : '';
-			new LocationSearch( input, function( data ) {
-				const hiddenDiv = document.getElementById( 'form-hidden-fields' );
-				const coordinates = document.createElement( 'input' );
-				coordinates.name = inputPrefix + 'coordinates';
-				coordinates.type = 'hidden';
-				coordinates.value = `${data.latitude},${data.longitude}`;
-				const timezone = document.createElement( 'input' );
-				timezone.name = inputPrefix + 'timezone';
-				timezone.type = 'hidden';
-				timezone.value = data.timezone;
-				hiddenDiv.appendChild( coordinates );
-				hiddenDiv.appendChild( timezone );
-			}, {clientId: CLIENT_ID, persistKey: inputPrefix});
+			const inputPrefix = input.dataset.location_input_prefix ?
+				input.dataset.location_input_prefix + '_' :
+				'';
+			new LocationSearch(
+				input,
+				function( data ) {
+					const hiddenDiv =
+						document.getElementById( 'form-hidden-fields' );
+					const coordinates = document.createElement( 'input' );
+					coordinates.name = inputPrefix + 'coordinates';
+					coordinates.type = 'hidden';
+					coordinates.value = `${data.latitude},${data.longitude}`;
+					const timezone = document.createElement( 'input' );
+					timezone.name = inputPrefix + 'timezone';
+					timezone.type = 'hidden';
+					timezone.value = data.timezone;
+					hiddenDiv.appendChild( coordinates );
+					hiddenDiv.appendChild( timezone );
+				},
+				{ clientId: CLIENT_ID, persistKey: `${inputPrefix}loc` }
+			);
 		});
 	}
 }
